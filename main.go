79c045f4b73df02a1aeb7a2cb5d01a0a1c740422
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2021-03-13 12:18:19 +1100

    Kinda working version

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 	"os/exec"
@@ -22,23 +23,26 @@ func sh(cmdName string, args ...string) {
 }
 
 func main() {
-	targetDevice = "/dev/sdb"
-  rootPartition = targetDevice+ "1"
-  swapPartition = targetDevice+ "2"
-  bootPartition = targetDevice+ "2"
+
+	targetDevice := flag.String("device", "", "Device to use ")
+	flag.Parse()
+
+	rootPartition := *targetDevice + "1"
+	swapPartition := *targetDevice + "2"
+	bootPartition := *targetDevice + "3"
 	sh("ls")
-	sh("parted", targetDevice, "--", "mklabel", "gpt")
-	sh("parted", targetDevice, "--", "mkpart", "primary", "512MiB", "-8GiB")
-	sh("parted", targetDevice, "--", "mkpart", "primary", "linux-swap", "-8GiB", "100%")
-	sh("parted", targetDevice, "--", "mkpart", "ESP", "fat32", "1MiB", "512MiB")
-	sh("parted", targetDevice, "--", "set", "3", "esp", "on")
+	sh("parted", *targetDevice, "--", "mklabel", "gpt")
+	sh("parted", *targetDevice, "--", "mkpart", "primary", "512MiB", "-8GiB")
+	sh("parted", *targetDevice, "--", "mkpart", "primary", "linux-swap", "-8GiB", "100%")
+	sh("parted", *targetDevice, "--", "mkpart", "ESP", "fat32", "1MiB", "512MiB")
+	sh("parted", *targetDevice, "--", "set", "3", "esp", "on")
 	sh("mkfs.ext4", rootPartition)
 	sh("mkswap", swapPartition)
 	sh("mkfs.fat", "-F", "32", "-n", "boot", bootPartition)
-	sh("mount", rootPartition ,"/mnt")
+	sh("mount", rootPartition, "/mnt")
 	sh("mkdir", "-p", "/mnt/boot")
-	sh("mount", bootPartition ,"/mnt/boot")
-	sh("swapon" swapPartition)
+	sh("mount", bootPartition, "/mnt/boot")
+	sh("swapon", swapPartition)
 	sh("nixos-generate-config", "--root", "/mnt")
 	sh("nixos-install")
 
