68709417bc60be7f54d90faca468e5f444101a87
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2024-09-08 15:04:05 +1000

    nixfmt-rfc-style

--- flake.nix
+++ flake.nix
@@ -4,24 +4,35 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
-    let out = system:
-      let pkgs = nixpkgs.legacyPackages."${system}";
-      in
-      {
-        defaultPackage = pkgs.stdenv.mkDerivation {
-          name = "nixos-installer";
-          src = ./.;
-          buildPhase = ''
-            HOME=$TMPDIR
-            ${pkgs.go}/bin/go build -o nixos-installer main.go
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    let
+      out =
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages."${system}";
+        in
+        {
+          defaultPackage = pkgs.stdenv.mkDerivation {
+            name = "nixos-installer";
+            src = ./.;
+            buildPhase = ''
+              HOME=$TMPDIR
+              ${pkgs.go}/bin/go build -o nixos-installer main.go
 
-          '';
-          installPhase = ''
-            mkdir -p $out/bin
-            mv nixos-installer $out/bin
-          '';
+            '';
+            installPhase = ''
+              mkdir -p $out/bin
+              mv nixos-installer $out/bin
+            '';
+          };
         };
-      }; in with utils.lib; eachSystem defaultSystems out;
+    in
+    with utils.lib;
+    eachSystem defaultSystems out;
 
 }
